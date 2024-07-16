2614c95e64ed0551a1ec42697717b990e8ba171e
bredamatt <28816406+bredamatt@users.noreply.github.com>
2024-07-16 01:27:38 +0100

    cargo fmt

--- ai-forensics-ebpf/src/main.rs
+++ ai-forensics-ebpf/src/main.rs
@@ -1,16 +1,15 @@
 #![no_std]
 #![no_main]
 
-use core::ffi::c_int;
-use aya_log_ebpf::info;
 use aya_ebpf::{
-    maps::HashMap,
-    macros::{tracepoint, map, kprobe, kretprobe},
-    programs::{TracePointContext, ProbeContext},
     bindings::BPF_ANY,
-    helpers::{bpf_printk, bpf_ktime_get_ns, bpf_get_current_pid_tgid},
+    helpers::{bpf_get_current_pid_tgid, bpf_ktime_get_ns, bpf_printk},
+    macros::{kprobe, kretprobe, map, tracepoint},
+    maps::HashMap,
+    programs::{ProbeContext, TracePointContext},
 };
-
+use aya_log_ebpf::info;
+use core::ffi::c_int;
 
 #[kretprobe]
 pub fn kretprobe_malloc(ctx: ProbeContext) -> u32 {
@@ -26,13 +25,12 @@ fn try_kretprobe_malloc(ctx: ProbeContext) -> Result<u32, u32> {
     Ok(0)
 }
 
-
 #[map]
 static mut START_TIMES: HashMap<u64, u64> = HashMap::with_max_entries(1024, BPF_ANY);
 
 #[tracepoint]
 pub fn sys_enter_futex(ctx: TracePointContext) -> u32 {
-    match unsafe {try_sys_enter_futex(ctx) } {
+    match unsafe { try_sys_enter_futex(ctx) } {
         Ok(ret) => ret,
         Err(_) => 1,
     }
@@ -48,7 +46,7 @@ unsafe fn try_sys_enter_futex(ctx: TracePointContext) -> Result<u32, i64> {
 
 #[tracepoint]
 pub fn sys_exit_futex(ctx: TracePointContext) -> u32 {
-    match unsafe { try_sys_exit_futex(ctx)} {
+    match unsafe { try_sys_exit_futex(ctx) } {
         Ok(ret) => ret,
         Err(_) => 1,
     }
@@ -58,15 +56,18 @@ unsafe fn try_sys_exit_futex(ctx: TracePointContext) -> Result<u32, i64> {
     let pid = bpf_get_current_pid_tgid();
     let start_time = match START_TIMES.get(&pid) {
         Some(value) => value,
-        None => return Err(1)
+        None => return Err(1),
     };
     let end_time = bpf_ktime_get_ns();
-    
+
     let duration = end_time - start_time;
-    info!(&ctx, "futex call duration for pid {} was {} ns", pid, duration);
-    
+    info!(
+        &ctx,
+        "futex call duration for pid {} was {} ns", pid, duration
+    );
+
     START_TIMES.remove(&pid).map_err(|_| 1)?;
-    
+
     Ok(0)
 }
 
@@ -82,7 +83,6 @@ fn try_sys_enter_mmap(ctx: TracePointContext) -> Result<u32, u32> {
     info!(&ctx, "sys_enter_mmap invoked");
     Ok(0)
 }
-
 
 #[tracepoint]
 pub fn sys_exit_mmap(ctx: TracePointContext) -> u32 {
@@ -200,7 +200,6 @@ fn try_sys_exit_brk(ctx: TracePointContext) -> Result<u32, u32> {
     info!(&ctx, "sys_exit_brk invoked");
     Ok(0)
 }
-
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
